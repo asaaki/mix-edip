23d447613d9dae0d99e26908ec5359be93110caf
Christoph Grabo <asaaki@mannaz.cc>
2015-07-26 21:15:25 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,20 +1,58 @@
+Code.eval_file "tasks/readme.exs"
+
 defmodule Edip.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app:     :edip,
-      version: "0.1.0",
-      elixir:  "~> 1.0",
-      deps:    deps
+      app:         :edip,
+      name:        "edip",
+      version:     "0.1.0",
+      elixir:      "~> 1.0",
+      description: description,
+      docs:        &docs/0,
+      package:     package,
+      deps:        deps
     ]
   end
 
-  def application do
-    [applications: []]
+  def application, do: []
+
+  defp description do
+    """
+    Mix task to create a docker image of your application release.
+
+    More detailed information about release image packaging at:
+    https://github.com/asaaki/elixir-docker-image-packager
+    """
+  end
+
+  defp docs do
+    {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
+    [
+      source_ref: ref,
+      readme:     "README.md",
+      main:       "README"
+    ]
+  end
+
+  defp package do
+    [
+      files:        ["lib", "tasks", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Christoph Grabo"],
+      licenses:     ["MIT"],
+      links: %{
+        "GitHub": "https://github.com/asaaki/mix-edip",
+        "Docs":   "http://hexdocs.pm/edip",
+        "EDIP":   "https://github.com/asaaki/elixir-docker-image-packager"
+      }
+    ]
   end
 
   defp deps do
-    []
+    [
+      { :ex_doc,  "~> 0.7", only: :docs },
+      { :earmark, "~> 0.1", only: :docs }
+    ]
   end
 end
