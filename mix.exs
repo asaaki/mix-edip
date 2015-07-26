f8b0ce0de6fe14c315413d829bad556b6f370d70
Christoph Grabo <asaaki@mannaz.cc>
2015-07-26 23:24:49 +0200

    Bump version to v0.1.1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Edip.Mixfile do
     [
       app:         :edip,
       name:        "edip",
-      version:     "0.1.0",
+      version:     "0.1.1",
       elixir:      "~> 1.0",
       description: description,
       docs:        &docs/0,
